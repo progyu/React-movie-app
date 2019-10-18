3870edb6aedd1f2fef921792d369ef64e3abdfc1
progyu <rocket7101@gmail.com>
2019-10-18 09:10:40 +0900

    feat: Get movie api using async / await

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -3,47 +3,39 @@ import './App.css';
 import Movie from './Movie'
 
 class App extends Component {
-  state = {
-  }
+  state = {}
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies : [
-          {
-            title: "Matrix",
-            poster: "",
-          },
-          {
-            title: "Full Metal Jacket",
-            poster: "",
-          },
-          {
-            title: "Oldboy",
-            poster: "",
-          },
-          {
-            title: "Star Wars",
-            poster: "",
-          },
-        ]
-      })
-    }, 5000);
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map((movie) => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
     })
     return movies;
   }
 
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+      return fetch("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
+      .then(response => response.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err))
+  }
+
   render() {
-  return (
-    <div className="App">
-      {this.state.movies ? this._renderMovies() : 'Loading'}
-    </div>
-  );
+    return (
+      <div className="App">
+        {this.state.movies ? this._renderMovies() : 'Loading'}
+      </div>
+    );
   }
 }
 
