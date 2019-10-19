cdacad02adbf09f8d7a8d9b6fe3df860c0e8ce5e
progyu <rocket7101@gmail.com>
2019-10-19 20:59:27 +0900

    Updates

---
+++ precache-manifest.19016146f8d8b8c08ab48d5b9631aef9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f605778475810d13bd989790cbbd4f1a",
+    "url": "/React-movie-app/index.html"
+  },
+  {
+    "revision": "f8cc11c8c91808debdf4",
+    "url": "/React-movie-app/static/css/main.50d89aab.chunk.css"
+  },
+  {
+    "revision": "8c58bd99ab3066b5650e",
+    "url": "/React-movie-app/static/js/2.4ce5b65d.chunk.js"
+  },
+  {
+    "revision": "f8cc11c8c91808debdf4",
+    "url": "/React-movie-app/static/js/main.c9f17dc4.chunk.js"
+  },
+  {
+    "revision": "76ca566a80c6594402ef",
+    "url": "/React-movie-app/static/js/runtime-main.478ad4aa.js"
+  }
+]);
